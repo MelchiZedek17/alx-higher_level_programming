c0609f1b39a7d4e6687a0453faba7044a37249d0
Amagyei Eugene <eugeneprahamagyei@gmail.com>
2022-08-25 12:42:38 -0700

    Task 0 Fixed

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python3
-def add(a, b):
+#!/usr/bin/pythoni3
+if __name__ == "__main__":
+    from add_0 import add
+
     a = 1
     b = 2
-    print(f"{a} + {b} = {a + b}", end="\n")
-
-
-__name__ == "__main__"
+    print(f"{a} + {b} = {add(a, b)}")
