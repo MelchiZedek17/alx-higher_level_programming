afd36c4177e1ceb324c47382b76d93b8162d5b97
Amagyei Eugene <eugeneprahamagyei@gmail.com>
2022-08-26 04:04:08 -0700

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    length = len(sys.argv)
+
+    length = len(sys.argv) - 1
     if length == 0:
         print("0 arguments.")
     else:
         if length == 1:
-            print("{} argument:".format(length))
-        elif length >= 2:
+            print("1 argument:")
+        else:
             print("{} arguments:".format(length))
         for num in range(length):
             number = 1 + num
