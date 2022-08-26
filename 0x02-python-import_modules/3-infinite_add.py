e6600c23b9341f0ecbc2b8e73936f897a70e608c
Amagyei Eugene <eugeneprahamagyei@gmail.com>
2022-08-26 06:27:30 -0700

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
+    num_args = len(argv)
     total = 0
-    for number in argv:
-        if type(number) == int or type(number) == float:
-            total += number
+    for num_arg in range(1, num_args):
+        total += int(argv[num_arg])
     print("{}".format(total))
